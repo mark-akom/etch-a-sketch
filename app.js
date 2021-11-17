0410edfac9b322e43f37665fd34f10359c1e4b53
Mark Akom <mark.akom2019@gmail.com>
2021-11-17 14:58:46 +0000

    Modified the index.html file to include a random button and also added the necessary styles to the button and lastly added the all the codes for the random color hover effects.

--- app.js
+++ app.js
@@ -1,25 +1,67 @@
 // start of the app
 const appContainer = document.querySelector('.app-container');
-const clearButton = document.querySelector('.clear')
+const clearButton = document.querySelector('.clear');
+const rgbButton = document.querySelector('.rgb-color');
+let rgbColor = false;
+let innerDivs;
+
+
 // function to create a divs for the grids
-function makeDivs() {
-    let totalDivs = 16 * 16
+function makeDivs(gridNum = 16) {
+    let totalDivs = gridNum * gridNum;
+    appContainer.style.gridTemplateColumns = `repeat(${gridNum}, auto)`;
+    appContainer.style.gridTemplateRows = `repeat(${gridNum}, auto)`;
     for (let i = 1; i <= totalDivs; i++) {
         let div = document.createElement('div');
-        div.style.border = '1px solid purple';
-        div.addEventListener('mouseover', (e) => {
-            e.target.style.backgroundColor = 'black';
-        })
+        div.addEventListener('mouseover', changeBackground);
         appContainer.appendChild(div);
     }
 }
 
+function clearDivs() {
+    let appContainerDivs = appContainer.querySelectorAll('div');
+    appContainerDivs = Array.from(appContainerDivs);
+    appContainerDivs.forEach(div => {
+        appContainer.removeChild(div);
+    })
+}
+
+function changeBackground(e) {
+
+    if(rgbColor) {
+        let red = Math.floor(Math.random() * 256);
+        let green = Math.floor(Math.random() * 256);
+        let blue = Math.floor(Math.random() * 256);
+        let color = `rgb(${red}, ${green}, ${blue})`;
+        e.target.style.backgroundColor = color;
+        return;
+    }
+
+    e.target.style.backgroundColor = 'black';
+}
+
 makeDivs();
+
+
+// event listeners
+rgbButton.addEventListener('click', () => {
+    rgbColor = true;
+})
 
 clearButton.addEventListener('click', () => {
     const sketchDivs = appContainer.querySelectorAll('div');
     sketchDivs.forEach(sketchDiv => {
         sketchDiv.style.backgroundColor = "white";
     })
-    const userNumber = prompt('Number of squares per side? **Maximum is 100** ');
-})
+    let userNumber = prompt('Number of squares per side? **Maximum is 100** ');
+    // modified the app-container to reflect the user entered number;
+    userNumber = Number(userNumber);
+
+    if(userNumber === 0 || userNumber === NaN) {
+        return;
+    } else {
+        clearDivs();
+        makeDivs(userNumber);
+    }
+
+});
